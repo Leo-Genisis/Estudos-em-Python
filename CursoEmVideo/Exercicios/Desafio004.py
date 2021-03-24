495e4d0aa58d004e7fa9b673e50e42afa7e89299
Leonardo <novagenisissistemas@gmail.com>
2021-03-24 16:05:33 -0300

    Inicio Python

---
+++ CursoEmVideo/Exercicios/Desafio004.py
@@ -0,0 +1,15 @@
+# Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitido,
+# e todas as informações e todas as informações possiveis sobre ele
+
+msg = 'Digite algo que vamos dar informações sobre ele, pronto para começar ?'
+
+print(msg)
+
+algo = input('Digite algo: ')
+
+print(algo)
+print('O tipo do elemento digitado é: ')
+print(type(algo))
+
+
+
